d157510512d708d59d3b543c3ccc9b2f732833ed
BulBul <bulbul@BulBul.fritz.box>
2018-05-21 00:15:38 +0200

    bug fixes

--- includes/js/particles.js
+++ includes/js/particles.js
@@ -20,7 +20,7 @@ var pJS = function(tag_id, params){
     },
     particles: {
       number: {
-        value: 800,
+        value: 80,
         density: {
           enable: true,
           value_area: 800
