be37c0a171c0893374e5dc919f844c4b275bdffb
BulBul <bulbul@BulBul.fritz.box>
2018-05-20 21:35:13 +0200

    first commit

---
+++ server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"./services"
+	"golang.org/x/net/http2"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// redirect every http request to https
+	go http.ListenAndServe(services.Config.Address, http.HandlerFunc(services.Redirect))
+	mux := http.NewServeMux()
+
+	// serving style and js files(only if no Bootstrap CDM):
+	statics := http.FileServer(http.Dir(services.Config.Static))
+	mux.Handle("/includes/", http.StripPrefix("/includes/", statics))
+
+	// defined in server_routes.go
+	mux.HandleFunc("/", services.Index)
+	
+	mux.HandleFunc("/err", services.Err)
+	mux.HandleFunc("/about", services.About)
+
+	// defined in handler_functions.go
+	mux.HandleFunc("/signup_account", services.SignupAccount)
+	mux.HandleFunc("/authenticate", services.Authenticate)
+	mux.HandleFunc("/logout", services.Logout)
+	mux.HandleFunc("/delete_account", services.DelAccount)
+
+	// javascript calls:
+	mux.HandleFunc("/bruse", services.Bruse)
+
+	server := http.Server{
+		Addr:    services.Config.AddressSSL,
+		Handler: mux,
+	}
+	fmt.Println(services.Config.Address + " and " + services.Config.AddressSSL)
+	http2.ConfigureServer(&server, &http2.Server{})
+	log.Fatal(server.ListenAndServeTLS(services.Config.Encryptcl1, services.Config.Encryptcl2))
+}
